ee15d87ec987a9a8045413275eca70d7fbc869cc
Sincan Maulana <sincanmaulanaa@gmail.com>
2024-12-09 14:29:55 +0700

    Changes port

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def tentang():
 #     return render_template("server/dashboard_edit.html")
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=3000, debug=True)
